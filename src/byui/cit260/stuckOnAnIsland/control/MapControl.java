6709971c51dea674500676209d2bf1c1464c3901
Jeremy Taylor <tay16036@byui.edu>
2017-07-15 16:00:51 -0600

    7/15/2017 Modified MapControl

--- src/byui/cit260/stuckOnAnIsland/control/MapControl.java
+++ src/byui/cit260/stuckOnAnIsland/control/MapControl.java
@@ -25,13 +25,33 @@ public class MapControl {
         
         // create the map
         Map map = new Map();
-        Location[] locations = new Location[5];
+        Location[] locations = new Location[25];
         
-        locations[0] = new Location("City1");
-        locations[1] = new Location("City2");
-        locations[2] = new Location("Ciyt3");
-        locations[3] = new Location("City4");
-        locations[4] = new Location("City5");
+        locations[0] = new Location("Shipwreck Beach");
+        locations[1] = new Location("Home Camp");
+        locations[2] = new Location("North Mountain Range");
+        locations[3] = new Location("Tool Building Shop");
+        locations[4] = new Location("North Vegetable Paradise Forest");
+        locations[5] = new Location("North Fruit Haven Forest");
+        locations[6] = new Location("Northeast Freshwater Creek");
+        locations[7] = new Location("Central Mountain Range");
+        locations[8] = new Location("Emerald Rain Forest");
+        locations[9] = new Location("Fresh Water Creek");
+        locations[10] = new Location("Ancient Cave");
+        locations[11] = new Location("Central Vegetable Forest");
+        locations[12] = new Location("Central Fruit Forest");
+        locations[13] = new Location("West Central Beach");
+        locations[14] = new Location("West Mountain Range");
+        locations[15] = new Location("Cave of Darkness");
+        locations[16] = new Location("Mystery Rain Forest");
+        locations[17] = new Location("Southwest Vegetable Forest");
+        locations[18] = new Location("Southwest Fruit Forest");
+        locations[19] = new Location("South Mini Mountain Range");
+        locations[20] = new Location("South Fresh Water Creek");
+        locations[21] = new Location("Southeast Wind Cave");
+        locations[22] = new Location("Southeast Vegetable Forest");
+        locations[23] = new Location("Southeast Fruit Forest");
+        locations[24] = new Location("Raft Location");
         
         map.setLocations(locations);
                 
@@ -45,7 +65,27 @@ public class MapControl {
         locations[1].setRegularSceneType(new City2Scene());
         locations[2].setRegularSceneType(new City3Scene()); 
         locations[3].setRegularSceneType(new City4Scene()); 
-        locations[4].setRegularSceneType(new City5Scene()); 
+        locations[4].setRegularSceneType(new City5Scene());
+        locations[5].setRegularSceneType(new City1Scene());
+        locations[6].setRegularSceneType(new City2Scene());
+        locations[7].setRegularSceneType(new City3Scene()); 
+        locations[8].setRegularSceneType(new City4Scene()); 
+        locations[9].setRegularSceneType(new City5Scene());
+        locations[10].setRegularSceneType(new City1Scene());
+        locations[11].setRegularSceneType(new City2Scene());
+        locations[12].setRegularSceneType(new City3Scene()); 
+        locations[13].setRegularSceneType(new City4Scene()); 
+        locations[14].setRegularSceneType(new City5Scene());
+        locations[15].setRegularSceneType(new City1Scene());
+        locations[16].setRegularSceneType(new City2Scene());
+        locations[17].setRegularSceneType(new City3Scene()); 
+        locations[18].setRegularSceneType(new City4Scene()); 
+        locations[19].setRegularSceneType(new City5Scene());
+        locations[20].setRegularSceneType(new City1Scene());
+        locations[21].setRegularSceneType(new City2Scene());
+        locations[22].setRegularSceneType(new City3Scene()); 
+        locations[23].setRegularSceneType(new City4Scene()); 
+        locations[24].setRegularSceneType(new City5Scene());
     }
 
     /*static void moveActorsToStartingLocation(Map map) {
